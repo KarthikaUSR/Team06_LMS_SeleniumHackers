15b8316e7265faedf89187dc75556564794e0243
KarthikaUSR <Karthika.USR@gmail.com>
2024-10-27 17:08:53 -0700

    Tested

--- src/test/java/comPages/batchModule.java
+++ src/test/java/comPages/batchModule.java
@@ -9,21 +9,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import comUtils.driverManager;
 import comUtils.excelReader;
 import comUtils.commonFunctions;
-import comUtils.configReader;
+
 import comUtils.tableUIvalidation;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.internal.org.jline.utils.Log;
+
 import pojo.batchRecords;
 
 
